0201694a0963aa554e784104ee0a781d7cb8234c
Trevor <144501789+rogra4813@users.noreply.github.com>
2024-11-18 21:47:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import pip
+pip.main(["install", "openpyxl"])
 
 st.title("🎈 EJERCICIO PRÁCTICO 3   ******GRUPO 6******")
 pd = pd.read_excel('cryptos.xlsx')
